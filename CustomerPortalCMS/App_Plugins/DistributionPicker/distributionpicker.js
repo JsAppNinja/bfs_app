c9c8393da196f9d969ca518eec5175d4cc3b212e
renanteabril <renanteabril@gmail.com>
2019-06-17 14:53:52 +0800

    reworked distributions properties to allow toggle on/off

---
+++ CustomerPortalCMS/App_Plugins/DistributionPicker/distributionpicker.js
@@ -0,0 +1,66 @@
+﻿angular.module("umbraco").controller("distributionPickerCtrl", function ($scope, contentResource) {
+
+    var PARENT_PAGE = 1919;
+
+    $scope.model.hideLabel = true;
+
+    $scope.items = [];
+
+    if(!$scope.model.value)
+    {
+        $scope.model.value = {
+            suppliers: [],
+            distributions: []
+        }
+    }
+
+    contentResource.getChildren(PARENT_PAGE).then(function (result) {
+        angular.forEach(result.items, function(distribution, index){
+            if(distribution.published) { // get published only
+                var item = {};
+                item.name = distribution.name;
+                item.id = distribution.id;
+
+                contentResource.getChildren(distribution.id).then(function(result){
+                    item.children = result.items
+                        .filter(supplier => supplier.published) // get published only
+                        .map(function(supplier){
+                        return {
+                            name: supplier.name,
+                            id: supplier.id
+                        }
+                    });
+                    $scope.items.push(item);
+                });    
+            }
+        });
+    });
+
+    $scope.toggleSupplierSelection = function(id){
+        var idx = $scope.model.value.suppliers.indexOf(id);
+
+        // Is currently selected
+        if (idx > -1) {
+          $scope.model.value.suppliers.splice(idx, 1);
+        }
+
+        // Is newly selected
+        else {
+          $scope.model.value.suppliers.push(id);
+        }
+    }
+
+    $scope.toggleDistributionSelection = function(id){
+        var idx = $scope.model.value.distributions.indexOf(id);
+
+        // Is currently selected
+        if (idx > -1) {
+            $scope.model.value.distributions.splice(idx, 1);
+        }
+
+        // Is newly selected
+        else {
+            $scope.model.value.distributions.push(id);
+        }
+    }
+});
---
+++ CustomerPortalCMS/Controllers/Api/ToolsApiController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Core.Models;
+using Umbraco.Web.WebApi;
+
+namespace CustomerPortalCMS.Controllers.Api
+{
+    public class ToolsApiController : UmbracoAuthorizedApiController
+    {
+
+        [HttpGet]
+        public int UnpublishedNodes()
+        {
+            var cs = Services.ContentService;
+            const int bldrLocationsId = 1264;
+            var items = cs.GetById(bldrLocationsId).Children();
+
+            return items.Count(x => !x.Published);
+        }
+
+        [HttpGet]
+        public string Migrate()
+        {
+
+            
+
+            var cs = Services.ContentService;
+            var cts = Services.ContentTypeService;
+
+            const int bldrLocationsId = 1264;
+            const string locationListItemAlias = "locationListItem";
+            var bldrLocations = cs.GetById(bldrLocationsId);
+
+            var distributions = string.Join(",", Umbraco.TypedContent(1919).Children.Select(x => x.Id));
+
+            var locationListItems = bldrLocations.Children().Where(x => x.ContentType.Alias == locationListItemAlias);
+
+            var locationListItemsDocType = cts.GetContentType(locationListItemAlias);
+
+            var propertyGroup = locationListItemsDocType.PropertyGroups.FirstOrDefault(x => x.Name == "Distribution");
+
+            List<int> primaryList = new List<int>();
+            
+            foreach (var locationListItem in locationListItems)
+            {
+                List<int> values = new List<int>();
+
+                foreach (var property in propertyGroup.PropertyTypes)
+                {
+                    if (locationListItem.GetValue(property.Alias) != null)
+                    {
+                        var selectedItems = locationListItem.GetValue<string>(property.Alias).Split(new [] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach(var selectedItem in selectedItems)
+                        {
+                            var content = cs.GetById(new Guid(selectedItem.Replace("umb://document/", "")));
+                            values.Add(content.Id);
+                        }
+                    }
+                }
+
+                
+                locationListItem.SetValue("suppliers", $"{{\"suppliers\":[{string.Join(",", values)}],\"distributions\":[{distributions}]}}");
+                if (locationListItem.Published)
+                {
+                    cs.SaveAndPublishWithStatus(locationListItem);
+                }
+                else
+                {
+                    cs.Save(locationListItem);
+                }
+            }
+
+            return "done";
+        }
+    }
+}
